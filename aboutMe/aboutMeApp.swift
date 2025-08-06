23297d76e4085cee37cf5556cbd933a43a5eab72
Elaina Foley <emtfoley1@Gmail.com>
2025-08-06 13:09:15 -0700

    Initial Commit

---
+++ aboutMe/aboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeApp.swift
+//  aboutMe
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
